eafd723ee8b98929441aaf27e605164e36f562d2
rajkumarreddy46 <rajkumarreddy46@gmail.com>
2024-09-02 07:59:37 +0000

    added new lines

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 # app.py
 def hello_world(name="World"):
     return f"Hello, {name}!"
+def goodbye_world(name="World"):
+    return f"Goodbye, {name}!"
 if __name__ == "__main__":
     print(hello_world())
     print(hello_world("Alice"))
+    print(goodbye_world())
+    print(goodbye_world("Alice"))
